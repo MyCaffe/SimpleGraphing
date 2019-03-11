0c23df9ba5636228c7caaa53611357f1a8afebb4
ZoroDerVonCodier <daveb@signalpop.com>
2019-03-10 19:08:47 -0700

    Added custom module location management.

---
+++ SimpleGraphing/Modules.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGraphing
+{
+    public class Modules
+    {
+        public Modules()
+        {
+        }
+
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        public static string CustomGraphingDirectory
+        {
+            get
+            {
+                string strPath = Modules.AssemblyDirectory;
+                strPath = strPath.TrimEnd('\\');
+                strPath += "\\CustomGraphing";
+
+                return strPath;
+            }
+        }
+
+        public static bool CustomGraphingExists(string strName)
+        {
+            string strPath = CustomGraphingDirectory + "\\" + strName + ".dll";
+            return File.Exists(strPath);
+        }
+    }
+}
