b481e9dc0fd7c88c23f42c55a455e13ff1c3b604
ZoroDerVonCodier <daveb@signalpop.com>
2019-08-10 16:44:05 -0700

    Fixed bugs with data params.

--- SimpleGraphing/GraphRender/GraphRenderLine.cs
+++ SimpleGraphing/GraphRender/GraphRenderLine.cs
@@ -24,11 +24,11 @@ namespace SimpleGraphing.GraphRender
             renderActions(g, dataset, nLookahead);
         }
 
-        private float getYValue(Plot p, double dfMin, double dfMax, double dfPMin, double dfPMax)
+        private float getYValue(Plot p, double dfMin, double dfMax, double dfPMin, double dfPMax, string strDataParam)
         {
             double fY = p.Y;
 
-            if (m_config.DataParam != null)
+            if (strDataParam != null)
             {
                 double? dfP = p.GetParameter(m_config.DataParam);
                 fY = dfP.GetValueOrDefault(0);
@@ -55,15 +55,20 @@ namespace SimpleGraphing.GraphRender
             double dfMinX = 0;
             double dfMaxX = 0;
             double dfMinY = 0;
-            double dfMaxY = 0;
+            double dfMaxY = 1;
             double dfParamMin = 0;
             double dfParamMax = 0;
-
+            string strDataParam = null;
 
             if (!string.IsNullOrEmpty(m_config.DataParam))
             {
+                string[] rgstr = m_config.DataParam.Split(':');
+                strDataParam = rgstr[0];
+
                 plots.GetParamMinMax(m_config.DataParam, out dfParamMin, out dfParamMax);
-                plots.GetMinMaxOverWindow(0, plots.Count, out dfMinX, out dfMinY, out dfMaxX, out dfMaxY);
+
+                if (rgstr.Length > 1 && rgstr[1] == "r")
+                    plots.GetMinMaxOverWindow(0, plots.Count, out dfMinX, out dfMinY, out dfMaxX, out dfMaxY);
 
                 double dfRange = dfParamMax - dfParamMin;
                 double dfOffset = dfRange * 0.05;
@@ -79,14 +84,19 @@ namespace SimpleGraphing.GraphRender
                 {
                     Plot plot = plots[nStartIdx + i];
                     float fX = rgX[i];
-                    float fY = getYValue(plot, dfMinY, dfMaxY, dfParamMin, dfParamMax);
+                    float fY = getYValue(plot, dfMinY, dfMaxY, dfParamMin, dfParamMax, strDataParam);
+
+                    if (float.IsNaN(fY) || float.IsInfinity(fY))
+                        fY = fYLast;
 
                     if (plotLast != null && plotLast.Active && plot.Active && ((plot.LookaheadActive && m_config.LookaheadActive) || i < rgX.Count - nLookahead))
                         g.DrawLine(m_style.LinePen, fXLast, fYLast, fX, fY);
 
                     plotLast = plot;
                     fXLast = fX;
-                    fYLast = fY;
+
+                    if (!float.IsNaN(fY) && !float.IsInfinity(fY))
+                        fYLast = fY;
                 }
             }
 
