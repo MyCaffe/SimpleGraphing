38f866dbf0fb9a028d140ea8052801bf5f6c7622
ZoroDerVonCodier <daveb@signalpop.com>
2019-11-06 11:38:23 -0800

    Added QuickRender

--- SimpleGraphing/SimpleGraphingControl.cs
+++ SimpleGraphing/SimpleGraphingControl.cs
@@ -399,6 +399,41 @@ namespace SimpleGraphing
         {
             pbImage.Invalidate();
         }
+
+        public static Image QuickRender(PlotCollection col)
+        {
+            SimpleGraphingControl simpleGraphingControl1 = new SimpleGraphingControl();
+            simpleGraphingControl1.Name = "SimpleGraphing";
+
+            simpleGraphingControl1.Configuration = new Configuration();
+            simpleGraphingControl1.Configuration.Frames.Add(new ConfigurationFrame());
+            simpleGraphingControl1.EnableCrossHairs = true;
+            simpleGraphingControl1.Configuration.Frames[0].XAxis.LabelFont = new Font("Century Gothic", 7.0f);
+            simpleGraphingControl1.Configuration.Frames[0].XAxis.Visible = true;
+            simpleGraphingControl1.Configuration.Frames[0].XAxis.Margin = 100;
+            simpleGraphingControl1.Configuration.Frames[0].YAxis.LabelFont = new Font("Century Gothic", 7.0f);
+            simpleGraphingControl1.Configuration.Frames[0].YAxis.Decimals = 3;
+            simpleGraphingControl1.Configuration.Frames[0].Plots.Add(new ConfigurationPlot());
+
+            if (col.Count > 0 && col[0].Y_values.Count == 4)
+            {
+                simpleGraphingControl1.Configuration.Frames[0].Plots[0].PlotType = ConfigurationPlot.PLOTTYPE.CANDLE;
+                simpleGraphingControl1.Configuration.Frames[0].XAxis.ValueType = ConfigurationAxis.VALUE_TYPE.TIME;
+            }
+            else
+            {
+                simpleGraphingControl1.Configuration.Frames[0].Plots[0].PlotType = ConfigurationPlot.PLOTTYPE.LINE;
+                simpleGraphingControl1.Configuration.Frames[0].XAxis.ValueType = ConfigurationAxis.VALUE_TYPE.NUMBER;
+            }
+
+            simpleGraphingControl1.Configuration.Frames[0].EnableRelativeScaling(true, true);
+
+            PlotCollectionSet set = new PlotCollectionSet();
+            set.Add(col);
+            List<PlotCollectionSet> rgSet = new List<PlotCollectionSet>() { set };
+            simpleGraphingControl1.BuildGraph(rgSet);
+            return simpleGraphingControl1.Render(600, 300);
+        }
     }
 
     public class Crosshairs
