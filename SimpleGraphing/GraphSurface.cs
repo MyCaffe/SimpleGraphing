978ae0df6055e4b88ab8a239ec4c83a49477488f
Dave Brown <daveb@signalpop.com>
2022-02-24 23:26:12 -0800

    Revert "Removed rendering flag."
    This reverts commit 38dc49f0ce53c7f97aa6ada19b1d3a68f33b0c58.

--- SimpleGraphing/GraphSurface.cs
+++ SimpleGraphing/GraphSurface.cs
@@ -16,7 +16,6 @@ namespace SimpleGraphing
         Rectangle m_rcBounds = new Rectangle();
         Bitmap m_bmp = null;
         GraphicsEx m_graphics = new GraphicsEx();
-        bool m_bRendering = false;
 
         public GraphSurface(ModuleCache cache)
         {
@@ -179,7 +178,7 @@ namespace SimpleGraphing
             int nWidth1 = nWidth - (nMargin * 2);
             int nHeight1 = 0;
 
-            for (int i = 0; i < m_frames.Count; i++)
+            for (int i=0; i<m_frames.Count; i++)
             {
                 GraphFrame frame = m_frames[i];
 
